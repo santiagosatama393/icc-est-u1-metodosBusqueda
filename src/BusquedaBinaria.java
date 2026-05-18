6aae126b4e010cae5421af03df788cfc01d25c82
santiagosatama393 <santiagosatama393@gmail.com>
2026-05-18 08:34:04 -0500

    frist commit

---
+++ src/BusquedaBinaria.java
@@ -0,0 +1,15 @@
+import models.Product;
+
+public class BusquedaBinaria {
+
+    public int findByName(Product[] products, String nombre){
+        
+        return -1;
+
+        int bajo = 0;
+        int alto =products.length -1;
+        while (bajo <= alto) {
+        
+         }
+    }
+}
